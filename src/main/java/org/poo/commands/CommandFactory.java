ca077945da2f38588612e9c97664473ff277aec7
RoxanaMaryB <roxana_mary.baranga@stud.acs.upb.ro>
2024-12-10 21:44:14 +0200

    Create accounts, cards. Add funds.

--- src/main/java/org/poo/commands/CommandFactory.java
+++ src/main/java/org/poo/commands/CommandFactory.java
@@ -8,20 +8,15 @@ import org.poo.fileio.CommandInput;
 
 public class CommandFactory {
     public static CommandStrategy createCommand(CommandInput commandInput) {
-        switch (commandInput.getCommand()) {
-            case "printUsers":
-                return new PrintUsersCommand(commandInput.getTimestamp());
-            case "addAccount":
-                return new AddAccountCommand(commandInput.getEmail(), commandInput.getCurrency(),
-                        commandInput.getAccountType(), commandInput.getTimestamp());
-            case "addFunds":
-                return new AddFundsCommand(commandInput.getAccount(), commandInput.getAmount(),
-                        commandInput.getTimestamp());
-            case "createCard":
-                return new CreateCardCommand(commandInput.getAccount(), commandInput.getEmail(),
-                        commandInput.getTimestamp());
-            default:
-                return new NoCommand();
-        }
+        return switch (commandInput.getCommand()) {
+            case "printUsers" -> new PrintUsersCommand(commandInput.getTimestamp());
+            case "addAccount" -> new AddAccountCommand(commandInput.getEmail(), commandInput.getCurrency(),
+                    commandInput.getAccountType(), commandInput.getTimestamp());
+            case "addFunds" -> new AddFundsCommand(commandInput.getAccount(), commandInput.getAmount(),
+                    commandInput.getTimestamp());
+            case "createCard" -> new CreateCardCommand(commandInput.getAccount(), commandInput.getEmail(),
+                    commandInput.getTimestamp());
+            default -> new NoCommand();
+        };
     }
 }
